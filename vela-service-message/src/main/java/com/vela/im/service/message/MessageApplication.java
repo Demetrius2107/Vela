d51f9dd16e3b608c9f348d1dc20b130f92d9d013
shanyuehua7 <2562017477@qq.com>
2026-08-09 01:49:30 +0800

    fix(im-service): MessageApplication scanBasePackages 添加 conversation 包

--- vela-service-message/src/main/java/com/vela/im/service/message/MessageApplication.java
+++ vela-service-message/src/main/java/com/vela/im/service/message/MessageApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages = {"com.vela.im.service.message", "com.vela.im.service.common", "com.vela.im.shared"})
+@SpringBootApplication(scanBasePackages = {"com.vela.im.service.message", "com.vela.im.service.common", "com.vela.im.service.conversation", "com.vela.im.shared"})
 @EnableFeignClients
 @MapperScan({"com.vela.im.service.message.infrastructure.persistence.mapper", "com.vela.im.service.common.entity"})
 public class MessageApplication {
